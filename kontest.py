8ec10c253521b1b5fcd5255878d4c954ec1042c3
Liliya-mama6 <vozdvizhenka@mail.ru>
2024-10-12 20:28:19 +0500

    Update kontest.p

--- kontest.py
+++ kontest.py
@@ -1,26 +1,26 @@
-from random import choice
-first = 'Мама мыла раму'
-second = 'Рамена мало было'
-ret=lambda fort, dort: fort==dort
-result=list(map(ret, first, second))
-print(result)
-def get_advanced_writer(file_name):
-    def write_everything(*data_set):
-        for i in range(len(data_set)):
-            with open(file_name, 'a') as file:
-                if i!=0:
-                    file.write('\n'+str(data_set[i]))
-                elif i==0:
-                    file.write(str(data_set[i]))
-    return write_everything
-write = get_advanced_writer('example.txt')
-write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
-class MysticBall:
-    def __init__(self, *spisok):
-        self.spisok=spisok
-    def __call__(self, *spisok):
-        return choice(self.spisok)
-first_ball = MysticBall('Да', 'Нет', 'Наверное')
-print(first_ball())
-print(first_ball())
-print(first_ball())
+from datetime import datetime
+import multiprocessing
+
+def read_info(name):
+    all_data=[]
+    with open(name, 'r') as file:
+        k = file.readline()
+        while k != '':
+            all_data.append(k)
+            k = file.readline()
+
+
+start = datetime.now()
+read_info('file 1.txt')
+read_info('file 2.txt')
+read_info('file 3.txt')
+read_info('file 4.txt')
+end = datetime.now()
+print(end - start)
+filenames = [f'./file {number}.txt' for number in range(1, 5)]
+if __name__ == '__main__':
+    with multiprocessing.Pool(processes=4) as pool:
+        start = datetime.now()
+        pool.map(read_info, filenames)
+    end = datetime.now()
+    print(end - start)
