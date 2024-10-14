a7cf8321178024432b8d41e25ae186ffa12ded5b
Liliya-mama6 <vozdvizhenka@mail.ru>
2024-10-14 20:16:38 +0500

    Update kontest.p

--- kontest.py
+++ kontest.py
@@ -9,18 +9,18 @@ def read_info(name):
             all_data.append(k)
             k = file.readline()
 
-
-start = datetime.now()
-read_info('file 1.txt')
-read_info('file 2.txt')
-read_info('file 3.txt')
-read_info('file 4.txt')
-end = datetime.now()
-print(end - start)
-filenames = [f'./file {number}.txt' for number in range(1, 5)]
 if __name__ == '__main__':
-    with multiprocessing.Pool(processes=4) as pool:
-        start = datetime.now()
-        pool.map(read_info, filenames)
+    start = datetime.now()
+    read_info('file 1.txt')
+    read_info('file 2.txt')
+    read_info('file 3.txt')
+    read_info('file 4.txt')
     end = datetime.now()
-    print(end - start)
+    time=end - start
+    filenames = [f'./file {number}.txt' for number in range(1, 5)]
+    a = datetime.now()
+    with multiprocessing.Pool() as pool:
+        pool.map(read_info, filenames)
+    b = datetime.now()
+    print(time)
+    print(b-a)
